ee8e1ba2a3f4a3febb333423ced688502d364471
Sheggy1 <pschenichniyvitaliy@gmail.com>
2022-02-18 02:23:01 +0200

    Done stopwatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [time, setTime] = React.useState(0);
+  const [timerOn, setTimeOn] = React.useState(false);
+
+  React.useEffect(() => {
+    let interval = null;
+
+    if(timerOn){
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10)
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+
+  },[timerOn]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="StopWatch">
+    <h1>StopWatch</h1>
+      <div>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+    </div>
+
+      <div>
+        <button onClick={() => setTimeOn(true)}>Start</button>
+        <button onClick={() => setTimeOn(false)}>Stop</button>
+        <button onClick={() => setTimeOn(false)}>Wait</button>
+        <button onClick={() => setTime(0)}>Reset</button>
+      </div>
     </div>
   );
 }
